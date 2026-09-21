de7be57c32ea1f52b154ad42b26def8a0cc24bb5
omran <ahmedesmail1101@gmail.com>
2026-09-21 12:02:45 +0300

    Adding Ahmed

--- image.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "Ahmed Ismail" << '\n';
-    cout << "Mahmoud Waleed" << '\n';
-    cout << "Mohamed Mokhtar" << '\n';
-    cout << "Ahmed Walid" << '\n';
-}
